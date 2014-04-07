80620604225f2697bc6e86ad0114f5c5f1a5e1cf
Opher Vishnia <opherv@gmail.com>
2014-04-08 00:03:17 +0300

    day2

---
+++ js/Trait.js
@@ -0,0 +1,33 @@
+var TraitInstance=function(parentTrait){
+    this.parentTrait=parentTrait;
+    this.value=0.5;
+}
+
+TraitInstance.prototype.constructor=TraitInstance;
+
+TraitInstance.prototype.getValue=function(propertyName){
+    //some properties are reverse proportional
+    var propValue = this.parentTrait[propertyName].reverse? 1-this.value: this.value;
+    return propValue*(this.parentTrait[propertyName].highLimit-this.parentTrait[propertyName].lowLimit)+this.parentTrait[propertyName].lowLimit;
+}
+
+TraitInstance.prototype.randomize=function(){
+    this.value=Math.random();
+};
+
+var baseTraits={
+    sizeSpeed: {
+        lowName: "Big & Slow",
+        highName: "Small & Fast",
+        speed: {
+            lowLimit: 30,
+            highLimit: 150,
+            reverse: true
+        },
+        size:{
+            lowLimit: 0.15,
+            highLimit: 0.7
+        }
+    }
+};
+
