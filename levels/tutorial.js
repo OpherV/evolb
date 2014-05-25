ed5657b708b2a60376a1ac2d4fbc402802630c02
Opher Vishnia <opherv@gmail.com>
2014-05-25 23:54:06 +0300

    work on tutorial

--- levels/tutorial.js
+++ levels/tutorial.js
@@ -101,18 +101,39 @@ var level=
             resolve();
         }))
 
-        .then(this.addTextGroup(["Yum!\nThat hit the the spot"]))
+       .then(this.addTextGroup(["Yum!\nThat hit the the spot"]))
 
-        .then(evolution.Level.Step(function(resolve,reject){
+       .then(evolution.Level.Step(function(resolve,reject){
             that.isControlEnabled=true;
             secondCreature.exists=true;
+            var proximityCheck=firstCreature.addProximityCheck(secondCreature,300);
+            proximityCheck.addOnce(function(){
+                firstCreature.removeProximityCheck(proximityCheck);
+                resolve();
+            },that);
+       }))
 
+       .then(evolution.Level.Step(function(resolve,reject){
+            that.isControlEnabled=false;
+            firstCreature.body.setZeroVelocity();
             resolve();
         }))
 
+       .then(this.addTextGroup(["Hey, here's another creature!",
+                                "Looks like he wants to join you",
+                                "To move more than one creature, hold the left mouse button",
+                                "The longer you hold your mouse button, the more creatures will respond"
+                                ]))
 
+       .then(evolution.Level.Step(function(resolve,reject){
+            that.isControlEnabled=true;
+            secondCreature.canBeControlled=true;
+            secondCreature.hasHunger=true;
+            secondCreature.canBob=true;
+            resolve();
+       }))
 
-        ;
+       ;
     },
     objects: [{"x":5500,"y":3400,"angle":0,"exists":false,"id":"food1","constructorName":"Food","layer":"powerUps"},{"x":5000,"y":3500,"angle":90,"exists":true,"id":"sprite_0","constructorName":"Rock","layer":"rocks","rockType":3},{"x":4700,"y":3500,"angle":45,"exists":true,"id":"sprite_1","constructorName":"Rock","layer":"rocks","rockType":2},{"x":4400,"y":3400,"angle":19,"exists":true,"id":"sprite_2","constructorName":"Rock","layer":"rocks","rockType":1},{"x":4200,"y":3100,"angle":168,"exists":true,"id":"sprite_3","constructorName":"Rock","layer":"rocks","rockType":1},{"x":4100,"y":2800,"angle":220,"exists":true,"id":"sprite_4","constructorName":"Rock","layer":"rocks","rockType":3},{"x":4300,"y":2500,"angle":20,"exists":true,"id":"sprite_5","constructorName":"Rock","layer":"rocks","rockType":2},{"x":4500,"y":2500,"angle":300,"exists":true,"id":"sprite_6","constructorName":"Rock","layer":"rocks","rockType":1},{"x":4820,"y":2500,"angle":90,"exists":true,"id":"sprite_7","constructorName":"Rock","layer":"rocks","rockType":3},{"x":5150,"y":2500,"angle":90,"exists":true,"id":"sprite_8","constructorName":"Rock","layer":"rocks","rockType":1},{"x":5350,"y":2700,"angle":75,"exists":true,"id":"sprite_9","constructorName":"Rock","layer":"rocks","rockType":3},{"x":5550,"y":2800,"angle":20,"exists":true,"id":"sprite_10","constructorName":"Rock","layer":"rocks","rockType":1},{"x":5700,"y":3050,"angle":80,"exists":true,"id":"sprite_11","constructorName":"Rock","layer":"rocks","rockType":2},{"x":5922.0001220703125,"y":3418.5000610351562,"angle":0,"exists":true,"id":"sprite_12","constructorName":"Rock1","layer":"rocks","rockType":1},{"x":5003.000183105469,"y":3690.4998779296875,"angle":-39.991367087596075,"exists":true,"id":"sprite_13","constructorName":"Rock3","layer":"rocks","rockType":3},{"x":6238.9990234375,"y":3476.4999389648438,"angle":-28.027564623591758,"exists":true,"id":"sprite_14","constructorName":"Rock2","layer":"rocks","rockType":2},{"x":5999.0008544921875,"y":3619.4998168945312,"angle":149.54455189382873,"exists":true,"id":"sprite_0","constructorName":"Rock","layer":"rocks"},{"x":4996.000061035156,"y":3911.4999389648438,"angle":0,"exists":true,"id":"sprite_1","constructorName":"Rock","layer":"rocks"},{"x":4920.999755859375,"y":4202.5,"angle":0,"exists":true,"id":"sprite_2","constructorName":"Rock","layer":"rocks"},{"x":6171.0003662109375,"y":3744.49951171875,"angle":117.7241353678611,"exists":true,"id":"sprite_3","constructorName":"Rock","layer":"rocks"},{"x":6307.999267578125,"y":3988.499755859375,"angle":-83.40513574601698,"exists":true,"id":"sprite_4","constructorName":"Rock","layer":"rocks"},{"x":5035,"y":4423.499755859375,"angle":-5.669612985869378,"exists":true,"id":"sprite_5","constructorName":"Rock","layer":"rocks"},{"x":4600,"y":3000,"angle":"0","exists":true,"id":"1st","constructorName":"Creature","layer":"creatures","hasHunger":false,"canBob":false,"canBreed":false,"baseTrait_sizeSpeed":"0.5"},{"x":5711.9427490234375,"y":4006.693115234375,"angle":0,"exists":false,"id":"2nd","constructorName":"Creature","layer":"creatures","canBreed":false,"canBob":true,"hasHunger":false,"baseTrait_sizeSpeed":"0.7","canBeControlled":false}]
 };
